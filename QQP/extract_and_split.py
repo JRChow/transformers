a65d846114901af251d395174ecafe51a09bd672
Jerome <zjrtomjohn@hotmail.com>
2019-11-05 13:53:48 -0800

    Split train & test

---
+++ QQP/extract_and_split.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+questions = pd.read_csv("questions.csv")
+
+questions.head()
+
+
+question_sample = questions.sample(n=100)
+
+train, test = train_test_split(question_sample, test_size=0.2)
+
+test.to_csv("test.csv")
+train.to_csv("")
